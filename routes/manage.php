bdebe5c8e68482e5181fd1e4d273edd84ccedbe3
josechanfm <josechan@ipm.edu.mo>
2023-05-11 18:05:55 +0800

    department list

--- routes/manage.php
+++ routes/manage.php
@@ -106,8 +106,8 @@ Route::middleware([
     'role:department',
 ])->group(function () {
     Route::prefix('/manage')->group(function(){
-        Route::get('department/{department}',[App\Http\Controllers\Department\DashboardController::class,'index']);
-        Route::resource('departments',App\Http\Controllers\Department\DepartmentController::class);
+        Route::get('department/{department}',[App\Http\Controllers\Department\DashboardController::class,'index'])->name('manage.department');
+        Route::resource('departments',App\Http\Controllers\Department\DepartmentController::class)->names('manage.departments');
         Route::resource('department/{department}/inquiries',App\Http\Controllers\Department\InquiryController::class)->names('manage.department.inquiries');
         Route::resource('mailers',App\Http\Controllers\MailerController::class)->names('manage.mailers');
     })->name('manage');
