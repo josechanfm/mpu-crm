f2a546a8923b1c8d5a15b91e558345450ffcdb0d
Wade Pak <P2001151@mpu.edu.mo>
2024-01-11 23:58:01 +0800

    try github pull request & temporary email blade

--- resources/views/emails/generalMail.blade.php
+++ resources/views/emails/generalMail.blade.php
@@ -6,6 +6,7 @@
 <body>
     <p>Testing email</p>
     {{ $body}}
-    {{ $token }}
+    p>To view your question, please follow this link:</p>
+    <a href="{{ url("/enquiry/ticket/{{$id}}/{{$token}}") }}">還想繼續問</a>
 </body>
 </html>
