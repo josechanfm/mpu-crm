a8806298a490a0012cf6fef09031a0193dfa0ab8
josechanfm <josechan@ipm.edu.mo>
2024-01-24 15:50:16 +0800

    merge change

--- resources/js/Layouts/DepartmentLayout.vue
+++ resources/js/Layouts/DepartmentLayout.vue
@@ -3,10 +3,10 @@
 
         <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible theme="light" width="250px" class="shadow-md " >
             <div class="m-4 text-center text-lg" v-if="collapsed">
-                <inertia-link href='/department'>MPU </inertia-link>
+                <inertia-link href='/'>MPU </inertia-link>
             </div>
             <div class="m-4 text-center text-lg" v-else>
-                <inertia-link href='/department'>MPU </inertia-link>
+                <inertia-link href='/'>MPU </inertia-link>
             </div>
 
             <DepartmentMenu :menuKeys='menuKeys'/>
