b5d39fabd493f39c263f5471b200c016e4a0954e
josechanfm <josechan@ipm.edu.mo>
2024-03-05 11:56:22 +0800

    other fields reset

--- resources/js/Pages/Enquiry/Form.vue
+++ resources/js/Pages/Enquiry/Form.vue
@@ -4,7 +4,6 @@
             <img src="/images/mpu_banner.png" width="300" />
         </a>
     </div>
-    {{ enquiry }}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
