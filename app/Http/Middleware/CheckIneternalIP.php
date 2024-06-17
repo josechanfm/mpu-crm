acc2073f69941317e753410da4c746cedaf596ae
josechanfm <josechan@ipm.edu.mo>
2024-06-17 12:20:44 +0800

    get realIP

--- app/Http/Middleware/CheckIneternalIP.php
+++ app/Http/Middleware/CheckIneternalIP.php
@@ -41,7 +41,6 @@ class CheckIneternalIP
             '202.175.24.0/24',
             '202.175.25.0/24',
         ];
-        dd($realIP);
         foreach($privateIPBlocks as $block){
             if(strpos($realIP, $block) == 0){
                 return true;
