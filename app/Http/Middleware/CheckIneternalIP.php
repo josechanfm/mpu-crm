e2107bedaede44c5deca6eaef8cd474933a65105
josechanfm <josechan@ipm.edu.mo>
2024-06-17 12:14:54 +0800

    get realIP

--- app/Http/Middleware/CheckIneternalIP.php
+++ app/Http/Middleware/CheckIneternalIP.php
@@ -17,7 +17,7 @@ class CheckIneternalIP
     public function handle(Request $request, Closure $next)
     {
         // Check if the client's IP address is an internal IP
-        if($this->isIneternalIP($request->ip())){
+        if($this->isIneternalIP($this->getRealClientIP($request->ip()))){
             return $next($request);
         }
         // If the IP is not inertinal, return a 403 Forbidden response
@@ -31,6 +31,8 @@ class CheckIneternalIP
      * @return bool
      */
     protected function isIneternalIP($ip){
+        $realIP=$this->getRealClientIP($ip);
+
         $privateIPBlocks=[
             '172.16.0.0/12',
             '202.175.6.0/24',
@@ -39,12 +41,19 @@ class CheckIneternalIP
             '202.175.24.0/24',
             '202.175.25.0/24',
         ];
-        dd($ip);
         foreach($privateIPBlocks as $block){
-            if(strpos($ip, $block) == 0){
+            if(strpos($realIP, $block) == 0){
                 return true;
             }
         }
         return false;
+    }
+
+    protected function getRealClientIP($ip){
+        $realIP=$ip;
+        if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR']){
+            $realIP= $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        return $realIP;
     }
 }
