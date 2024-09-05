92c10f589eec2c5f8fe1843e156f3159fc81abca
josechanfm <josechan@ipm.edu.mo>
2024-09-05 17:13:13 +0800

    quota count <=0

--- resources/js/Pages/Form/national_security.vue
+++ resources/js/Pages/Form/national_security.vue
@@ -41,7 +41,7 @@
                     :rules="[{ required: formFields['course_code'].required, message: '為必填欄位 is Required' }]">
                     <a-radio-group v-model:value="formData[formFields['course_code'].id]">
                         <template v-for="course in JSON.parse(formFields['course_code']['extra'])">
-                            <a-radio :style="radioStyle" :value="course.code" :disabled="vacancy(course.code) == 0">
+                            <a-radio :style="radioStyle" :value="course.code" :disabled="vacancy(course.code) <= 0">
                                 {{ course.code }} ( {{ vacancy(course.code) }} Availables )
                             </a-radio>
                         </template>
