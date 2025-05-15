773974cbf0121245aea2425b4da500b1890ffb1e
josechanfm <josechan@ipm.edu.mo>
2025-05-15 10:47:13 +0800

    config session cookies

---
+++ resources/js/Pages/Recruitment/Vacancies_footer_aca_en.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="row">
+        <ul class="p-10">
+            <li>
+                下載表格 :
+                <ul class="p-5">
+                    <li>
+                        <a href="https://www.ipm.edu.mo/cntfiles/upload/docs/common/personnel/application_pessoal_nao_docente.pdf" style="text-size-adjust: none; color: rgb(16, 74, 123);" target="_blank">非教職人員職位申請書</a></li>
+                    <li>
+                        <a href="https://www.ipm.edu.mo/cntfiles/upload/docs/vacancy/308b_fulltime_academicstaff.pdf" style="text-size-adjust: none; color: rgb(16, 74, 123);" target="_blank">教職人員職位申請書 (全職)</a></li>
+                    <li>
+                        <a href="https://www.ipm.edu.mo/cntfiles/upload/docs/vacancy/308c_nonfulltime_serviceprovider.pdf" style="text-size-adjust: none; color: rgb(16, 74, 123);" target="_blank">教學服務提供人員申請書 (非全職)</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="https://www.ipm.edu.mo/zh/academic_appointment.php" style="text-size-adjust: none; color: rgb(16, 74, 123);" target="_blank">教職人員服務條款</a></li>
+            <li>
+                <a href="https://bo.io.gov.mo/bo/i/99/34/despsaaej29_cn.asp#29" style="text-size-adjust: none; color: rgb(16, 74, 123);" target="_blank">澳門理工大學人事章程及教職人員章程</a></li>
+            <li>
+                <a href="https://www.ipm.edu.mo/cntfiles/upload/docs/vacancy/typhoon_arrangement_c.pdf" style="text-size-adjust: none; color: rgb(16, 74, 123);" target="_blank">懸掛颱風或暴雨訊號時 求職筆試／面試之安排</a></li>
+        </ul>
+        <div class="pl-10">
+            <table id="contact_box">
+                <tbody>
+                    <tr>
+                        <th>地址:</th>
+                        <td colspan="2">
+                            人事處<br/>
+                            澳門理工大學<br/>
+                            中國澳門高美士街<br/>
+                        </td>
+                    </tr>
+                    <tr >
+                        <th rowspan="2">電話:</th>
+                        <td colspan="2" style="border-right-style: solid; border-right-color: rgb(204, 229, 221);text-align: center;" >教職人員事務</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="border-right-style: solid; border-right-color: rgb(204, 229, 221);text-align: center;">
+                            (853) 8599 6125 / 6247</td>
+                    </tr>
+                    <tr>
+                        <th>傳真:</th>
+                        <td colspan="2">
+                            (853) 28702599</td>
+                    </tr>
+                    <tr>
+                        <th>電郵:</th>
+                        <td colspan="2">
+                            <a href="mailto:personnel@mpu.edu.mo" style="text-size-adjust: none; color: rgb(16, 74, 123);">personnel@mpu.edu.mo</a></td>
+                    </tr>
+                    <tr >
+                        <th rowspan="3">辦公時間:</th>
+                        <td>星期一至四</td>
+                        <td>09:00-13:00, 14:30-17:45</td>
+                    </tr>
+                    <tr>
+                        <td>星期五</td>
+                        <td>09:00-13:00, 14:30-17:30</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">星期六、日及公眾假期休息</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    </template>
+
+
+<style>
+#contact_box{
+	margin: 0px 0px 15px;
+	padding: 0px;
+	border-style: solid none none;
+	border-top-color: rgb(204, 229, 221);
+	border-right-width: initial;
+	border-right-color: initial;
+	border-bottom-width: initial;
+	border-bottom-color: initial;
+	border-left-width: initial;
+	border-left-color: initial;
+	outline: none;
+	line-height: 18px;
+	width: 625px;
+	border-collapse: collapse;
+	color: rgb(68, 68, 68);
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 15px;
+	letter-spacing: 1.6px;
+	background-color: rgb(221, 246, 248);
+}
+#contact_box th{
+	color: rgb(25, 134, 102);
+	border-right-style: solid;
+	border-right-color: rgb(204, 229, 221);
+	background: rgb(241, 249, 238);
+	vertical-align: top;
+	padding: 8px 15px;
+	border-bottom-style: solid;
+	border-bottom-color: rgb(204, 229, 221);
+	text-align: left; 
+}
+#contact_box td{
+	vertical-align: top;
+	padding: 8px 15px 8px 5px;
+	border-bottom-style: solid;
+	border-bottom-color: rgb(204, 229, 221);
+	background: rgb(243, 250, 245);
+}
+</style>
