e14e6aa0ae6793a8bface2d52342bbc426aa9470
josechanfm <josechan@ipm.edu.mo>
2025-08-26 10:23:50 +0800

    Update OrderConfirmed.vue

--- resources/js/Pages/Souvenir/OrderConfirmed.vue
+++ resources/js/Pages/Souvenir/OrderConfirmed.vue
@@ -1,16 +1,4 @@
 <template>
-    <template #header>
-        <div class="flex justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                預購畢業生限量專屬紀念品<br>
-                Pre-order of Graduation Gifts
-            </h2>
-            <div class="flex justify-end gap-2">
-                <div v-if="user" @click="logout">Logout<br>登出</div>
-                <div v-else @click="login">Login<br>登入</div>
-            </div>
-        </div>
-    </template>
     <div v-if="paymentData && Object.keys(paymentData).length">
         <form id="paymentForm" :action="paymentUrl" method="POST">
             <div v-for="(value, field) in paymentData" :key="field">
