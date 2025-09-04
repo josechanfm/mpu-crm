31f6d6dcdbc9c1e63b1c3c0917411c0c9ac42c23
josechanfm <josechan@ipm.edu.mo>
2025-09-04 16:13:59 +0800

    receipt format

--- resources/views/souvenir/receipt.blade.php
+++ resources/views/souvenir/receipt.blade.php
@@ -20,7 +20,7 @@
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-top: 20px;
+            margin-top: 10px;
         }
         th, td {
             border: 1px solid #ccc;
@@ -55,13 +55,13 @@
             <div>Pickup Code / 取件碼</div>
             <img style="float:right" width="150" src="{{ $pickupQrcodePath }}" alt="QR Code" />
         </div>
-        <div>Faculty/學院: {{ $order->form_meta->faculty  }}</div>
-        <div>Degree/學位: {{ $order->form_meta->degree  }}</div>
+        <div>Receipt No. / 收據編號: {{ $order->updated_at->format('y') }}-{{ str_pad($order->id, 6, '0', STR_PAD_LEFT) }}</div>
+        <div>Faculty/學院: {{ strtoupper($order->form_meta->faculty)  }}</div>
+        <div>Degree/學位: {{ ucfirst($order->form_meta->degree)  }}</div>
         <div>Phone/電話: {{ $order->form_meta->phone }}</div>
         <div>Email/電郵: {{ $order->form_meta->email  }}</div>
-        <div>Order ID / 訂單編號: {{ str_pad($order->id, 6, '0', STR_PAD_LEFT) }}</div>
-        <div>Order Date / 訂單日期: {{ $order->created_at->format('Y-m-d H:i:s') }}</div>
-        <p></p>
+        <div>Payment Date / 支付日期: {{ $order->created_at->format('Y-m-d') }}</div>
+        <div>Payment Method/支付方式: GOVPAY online (BOC) / 線上政付通(BOC)</div>
         <p></p>
     </div>
     <table>
@@ -92,8 +92,8 @@
     </table>
 
     <div class="footer">
-        <p>Thank you for your purchase! / 感謝您的購買！</p>
-        <p>For any inquiries, contact us at support@mpu.edu.mo / 如有任何查詢，請聯繫我們 support@mpu.edu.mo</p>
+        <p>Thank you for your purchase! <br> 感謝您的購買！</p>
+        <p>For any inquiries, contact us at sao@mpu.edu.mo <br> 如有任何查詢，請聯繫我們 sao@mpu.edu.mo</p>
     </div>
 </body>
 </html>
