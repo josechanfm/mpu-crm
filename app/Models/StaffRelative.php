845884aeb866b463be0c296351a428d2bc54800e
josechanfm <josechan@ipm.edu.mo>
2026-01-23 17:25:24 +0800

    Update StaffRelative.php

--- app/Models/StaffRelative.php
+++ app/Models/StaffRelative.php
@@ -8,7 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class StaffRelative extends Model
 {
     use HasFactory;
-
+    protected $fillable=[
+        'staff_id',
+        'has_allowance',
+        'has_medical',
+        'medical_num',
+        'staff_num',
+        'name_zh',
+        'name_pt',
+        'relationship',
+        'allowance_type',
+        'dob',
+        'id_num',
+        'medical_type',
+    ];
     public function staff()
     {
         return $this->belongsTo(Staff::class);
